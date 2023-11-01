f072244c0c25f538096e8b656ab94829d04fdd2b
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-01 17:49:01 +0000

    Entity core

--- src/main/java/com/gabriel/cleanarch/cleanarch/Entities/Loan.java
+++ src/main/java/com/gabriel/cleanarch/cleanarch/Entities/Loan.java
@@ -1,12 +1,25 @@
 package com.gabriel.cleanarch.cleanarch.Entities;
 
 public class Loan {
+
+    private int id;
+    private String name;
     private int rateLoan;
+    private int payment;
+
+    public void setId(int id) { this.id = id; }
+    public int getId() { return this.id; }
+
+    public void setName(String name) { this.name = name; }
+    public String getName() { return this.name; }
+
+    public void setPayment(int payment) { this.payment = payment; }
+    public int getPayment() { return this.payment; }
 
     public void setRateLoan(int rateLoan) { this.rateLoan = rateLoan; }
     public int getRateLoan() { return this.rateLoan; }
 
-    public int applyInterest(int monthOfPayment) {
-        return this.getRateLoan() * monthOfPayment;
+    public void applyInterest(int monthOfPayment) {
+        this.setPayment( (this.getRateLoan() * monthOfPayment) );
     }
 }
