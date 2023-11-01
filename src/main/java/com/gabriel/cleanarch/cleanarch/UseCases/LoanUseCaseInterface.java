6cdcf69f614edb4320ec32bd9811713f0e0b992c
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-01 17:49:23 +0000

    Interface use case

---
+++ src/main/java/com/gabriel/cleanarch/cleanarch/UseCases/LoanUseCaseInterface.java
@@ -0,0 +1,7 @@
+package com.gabriel.cleanarch.cleanarch.UseCases;
+
+import com.gabriel.cleanarch.cleanarch.Entities.Loan;
+
+public interface LoanUseCaseInterface {
+    Loan createLoan(Loan loan);
+}
