d53660725b2cb64110f04cc0d6376bf1006f27b1
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-03 02:37:15 +0000

    Refactor to correct layer

--- src/main/java/com/gabriel/cleanarch/cleanarch/Adapters/communication/LoanRepositoryAdapterInterface.java
+++
@@ -1,5 +0,0 @@
-package com.gabriel.cleanarch.cleanarch.Adapters.communication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepositoryAdapterInterface extends JpaRepository<LoanEntityAdapter, Long> { }
