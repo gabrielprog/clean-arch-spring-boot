0ae791dfe1ce4ba4aa903d6f2d7eee95bfe525ea
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-03 17:09:20 +0000

    Add correct dependecy inject

--- src/main/java/com/gabriel/cleanarch/cleanarch/Adapters/controllers/LoanControllerAdapter.java
+++ src/main/java/com/gabriel/cleanarch/cleanarch/Adapters/controllers/LoanControllerAdapter.java
@@ -1,12 +1,12 @@
 package com.gabriel.cleanarch.cleanarch.Adapters.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.cleanarch.cleanarch.Adapters.Mappers.MapperEntityLoan;
-import com.gabriel.cleanarch.cleanarch.Adapters.gateways.LoanGateway;
 import com.gabriel.cleanarch.cleanarch.Adapters.gateways.LoanGatewayAdapter;
 import com.gabriel.cleanarch.cleanarch.db.LoanEntityAdapter;
 import com.gabriel.cleanarch.cleanarch.Entities.Loan;
@@ -16,12 +16,14 @@ import com.gabriel.cleanarch.cleanarch.UseCases.LoanUseCaseInterface;
 @RestController
 @RequestMapping("/loan")
 public class LoanControllerAdapter {
+    
+    @Autowired
+    private LoanGatewayAdapter loanGateway;
      
     @PostMapping
     public LoanEntityAdapter create(@RequestBody LoanEntityAdapter loanRequest) {
         LoanUseCaseInterface loanUseCase = new LoanUseCase();
         Loan loan = loanUseCase.createLoan( MapperEntityLoan.toLoan(loanRequest) );
-        LoanGateway loanGateway = new LoanGatewayAdapter();
 
         return loanGateway.create(loan);
     }
