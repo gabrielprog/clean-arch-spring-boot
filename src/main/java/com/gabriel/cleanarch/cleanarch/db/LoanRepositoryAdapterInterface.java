9863d89e9b4ea802410e6124528e7bd9bbaab550
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-03 17:08:46 +0000

    Add annotation @Respository

--- src/main/java/com/gabriel/cleanarch/cleanarch/db/LoanRepositoryAdapterInterface.java
+++ src/main/java/com/gabriel/cleanarch/cleanarch/db/LoanRepositoryAdapterInterface.java
@@ -1,5 +1,7 @@
 package com.gabriel.cleanarch.cleanarch.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanRepositoryAdapterInterface extends JpaRepository<LoanEntityAdapter, Long> { }
