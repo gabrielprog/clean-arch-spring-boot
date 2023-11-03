827f669b455c63e2baf496f2c6f1bb5da19ab910
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-11-03 02:36:34 +0000

    Refactor to correct struct

--- src/main/java/com/gabriel/cleanarch/cleanarch/Adapters/controllers/LoanControllerAdapter.java
+++ src/main/java/com/gabriel/cleanarch/cleanarch/Adapters/controllers/LoanControllerAdapter.java
@@ -1,14 +1,14 @@
 package com.gabriel.cleanarch.cleanarch.Adapters.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.cleanarch.cleanarch.Adapters.Mappers.MapperEntityLoan;
-import com.gabriel.cleanarch.cleanarch.Adapters.communication.LoanEntityAdapter;
-import com.gabriel.cleanarch.cleanarch.Adapters.communication.LoanRepositoryAdapterInterface;
+import com.gabriel.cleanarch.cleanarch.Adapters.gateways.LoanGateway;
+import com.gabriel.cleanarch.cleanarch.Adapters.gateways.LoanGatewayAdapter;
+import com.gabriel.cleanarch.cleanarch.db.LoanEntityAdapter;
 import com.gabriel.cleanarch.cleanarch.Entities.Loan;
 import com.gabriel.cleanarch.cleanarch.UseCases.LoanUseCase;
 import com.gabriel.cleanarch.cleanarch.UseCases.LoanUseCaseInterface;
@@ -16,15 +16,13 @@ import com.gabriel.cleanarch.cleanarch.UseCases.LoanUseCaseInterface;
 @RestController
 @RequestMapping("/loan")
 public class LoanControllerAdapter {
-
-    @Autowired
-    private LoanRepositoryAdapterInterface loanRepositoryAdapterInterface;
      
     @PostMapping
     public LoanEntityAdapter create(@RequestBody LoanEntityAdapter loanRequest) {
         LoanUseCaseInterface loanUseCase = new LoanUseCase();
         Loan loan = loanUseCase.createLoan( MapperEntityLoan.toLoan(loanRequest) );
-        
-        return loanRepositoryAdapterInterface.save( MapperEntityLoan.toLoanAdapter(loan) );
+        LoanGateway loanGateway = new LoanGatewayAdapter();
+
+        return loanGateway.create(loan);
     }
 }
